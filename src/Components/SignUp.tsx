8818158cc40114a4194126a878085e8c6a563a2f
KristinWilliams <wkristin71@yahoo.co.uk>
2022-07-12 12:57:48 -0500

    completed confirm password functionality

--- src/Components/SignUp.tsx
+++ src/Components/SignUp.tsx
@@ -3,39 +3,31 @@ import styles from "./scss/SignUp.module.scss";
 // import { auth } from "../firebase";
 import { useNavigate } from "react-router-dom";
 import { UseUserContext } from "./Context";
-import { getDatabase, ref, set } from "firebase/database";
 import { auth } from "../firebase";
 
 const SignUp = () => {
   const emailRef = useRef<HTMLInputElement>(null);
   const passRef = useRef<HTMLInputElement>(null);
-  const nameRef = useRef<HTMLInputElement>(null);
+  const confirmPassRef = useRef<HTMLInputElement>(null);
   const navigate = useNavigate();
   const { createUser } = UseUserContext();
-  const database = getDatabase();
-
-  const createUsername = (
-    userId: string,
-    name: React.RefObject<HTMLInputElement>
-  ) => {
-    if (name.current) {
-      set(ref(database, "users/" + userId), {
-        username: name.current.value,
-      });
-    }
-  };
 
   const signUpOnClick = async (e: React.ChangeEvent<any>) => {
     e.preventDefault();
-    if (emailRef.current && passRef.current) {
+    if (emailRef.current && passRef.current && confirmPassRef.current) {
       const email = emailRef.current.value;
       const pass = passRef.current.value;
-      try {
-        const user = await createUser(auth, email, pass);
-        createUsername(user.user.uid, nameRef);
-        navigate("/dashboard");
-      } catch (error) {
-        console.log(error);
+      const confirmPass = confirmPassRef.current.value;
+      if (pass === confirmPass) {
+        try {
+          const user = await createUser(auth, email, pass);
+          console.log(pass, confirmPass);
+          navigate("/dashboard");
+        } catch (error) {
+          console.log(error);
+        }
+      } else {
+        alert("Passwords must match. ");
       }
     }
   };
@@ -46,7 +38,6 @@ const SignUp = () => {
       <div className={styles["form-container"]}>
         <h1>Sign Up Today!</h1>
         <form action="" className={styles["sign-up"]}>
-          <input type="text" ref={nameRef} placeholder="Name" required />
           <input type="email" ref={emailRef} placeholder="Email" required />
           <input
             type="password"
@@ -54,7 +45,12 @@ const SignUp = () => {
             placeholder="Password"
             required
           />
-          {/* <input type="password" placeholder="Confirm Password" required /> */}
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            ref={confirmPassRef}
+            required
+          />
           <button type="submit" onClick={signUpOnClick}>
             Sign Up
           </button>
