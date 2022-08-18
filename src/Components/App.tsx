b04af5c3205bd0c42e8ec06a6c72cc7fdd26a331
KristinWilliams <wkristin71@yahoo.co.uk>
2022-08-18 18:29:06 -0500

    finished mobile version for individual recipe page

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -3,6 +3,7 @@ import HomeScreen from "./HomeScreen";
 import SignUp from "./SignUp";
 import SignIn from "./SignIn";
 import Dashboard from "./Dashboard";
+import RecipePage from "./RecipePage";
 import base from "./base.scss";
 import { UserAuth } from "./Context";
 
@@ -15,7 +16,7 @@ const App = () => {
           <Route path="/" element={<HomeScreen />}></Route>
           <Route path="/sign-up" element={<SignUp />}></Route>
           <Route path="/sign-in" element={<SignIn />}></Route>
-          <Route path="/dashboard" element={<Dashboard />}></Route>
+          <Route path="/dashboard" element={<RecipePage />}></Route>
         </Routes>
       </UserAuth>
     </Router>
